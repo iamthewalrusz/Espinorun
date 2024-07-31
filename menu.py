52013ff03ce304a6fb6d8e0f0f171d06f4f08616
Nathan Guimarães <90988459+nathanhgo@users.noreply.github.com>
2024-07-31 11:35:49 -0300

    Recuperando arquivos

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from main_nova import game
+from main import game
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -11,7 +11,7 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-font = pygame.font.Font("./assets/Pixels.ttf", 25)
+font = pygame.font.Font("./assets/Pixels.ttf", 50)
 font_titulo = pygame.font.Font("./assets/Pixels.ttf", 150)
 
 def draw_text(text, font, color, surface, x, y):
@@ -27,26 +27,36 @@ pygame.mixer.music.load("./assets/teste.wav")
 pygame.mixer.music.play(loops=-1)
 
 def main_menu():
+    pontos = 0
+    record = 0
+    pygame.display.set_caption('Espinorun')
+    music_img = pygame.image.load('./assets/NotaMusical.png').convert_alpha()
     while True:
 
         screen.fill([201, 218, 191])
         draw_text("ESPINORUN", font_titulo, (255, 255, 255), screen, 435, 200)
+        draw_text("Corra, Espinossauro!", font, (255, 255, 255), screen, 490, 300)
         music_pause = 0 # Variável para pausar e despausar a música
+
+        score = font.render(f'Recorde: {int(record)}', True, (95, 111, 101))
+        screen.blit(score, (screen.get_width() - 200, 50))
         
         mx, my = pygame.mouse.get_pos()
 
         button_1 = pygame.Rect(525, 500, 200, 50)
-        button_music = pygame.Rect(25, 650, 50, 50)
+        button_music = pygame.Rect(25, 650, 60, 60)
         
         pygame.draw.rect(screen, (156, 169, 134), button_1)
         pygame.draw.rect(screen, (156, 169, 134), button_music)
-        draw_text("Iniciar", font, (255, 255, 255), screen, 605, 515)
+        draw_text("Iniciar", font, (255, 255, 255), screen, 580, 500)
 
         if button_1.collidepoint((mx, my)):
             pygame.draw.rect(screen, (128, 141, 124), button_1)
-            draw_text("Iniciar", font, (255, 255, 255), screen, 605, 515)
+            draw_text("Iniciar", font, (255, 255, 255), screen, 580, 500)
             if click:
-                game()
+                pontos = game()
+                if pontos >= record:
+                    record = pontos
 
         if button_music.collidepoint((mx, my)):
             pygame.draw.rect(screen, (128, 141, 124), button_music)
@@ -71,6 +81,8 @@ def main_menu():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
+
+        screen.blit(music_img, (screen.get_width() - 1245, 655))
         
         pygame.display.update()
         clock.tick(60)
